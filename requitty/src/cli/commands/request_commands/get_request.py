b43ea0aa056b4b4a4a1d807a4f81091363fbc7d6
daliondev <d4lion.dev@gmail.com>
2022-11-28 22:22:05 -0500

    :sparkles: Handling errors

--- requitty/src/cli/commands/request_commands/get_request.py
+++ requitty/src/cli/commands/request_commands/get_request.py
@@ -1,11 +1,12 @@
 import requests
-
+import json
 
 def main(params):
 
     if 'get' in params.request:
         try:
             response = requests.get(params.url)
+            response_decoded = response.json()
 
             ###############################################
             #######          ALL IS OK            #########
@@ -14,28 +15,21 @@ def main(params):
             if response.status_code >= 200 and response.status_code < 299: #If the status code is Ok 
                 
                 if params.verbose: #If verbose is true in params print the response
-                    print( response.json(), response.status_code)
-                print('Succes')
-
-
-
+                    print(response_decoded)
+                print(f'\n Succes and exit with {response.status_code}')
+            
 
 
             ###############################################
             ####          ERROR STATUS CODE          ######
             ###############################################
                 
-            elif response.status_code == 404:
-                print('Not found')
-                return 1
-            elif response.status_code == 500:
-                print('Internal server error')
-                return 1
-            else:
-                print('Error was ocurred o url is not valid')
-                return 1
+            elif response.status_code >= 400 or response.status_code >= 599:
+                print(f'\n Not found exit with {response.status_code} \n response: {response_decoded}' )
+                return response.status_code
         except:
-            return 'Error was ocurred'
+            print("\n Error was ocurred, check url and try again")
+            return 500
 
 
 if __name__ == '__main__':
