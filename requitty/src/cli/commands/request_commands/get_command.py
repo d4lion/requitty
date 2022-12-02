da2c9bf90c47d5f73ca389d3b0aa51daca688cca
daliondev <d4lion.dev@gmail.com>
2022-12-02 17:36:35 -0500

    feat(word): change get arg

--- requitty/src/cli/commands/request_commands/get_command.py
+++ requitty/src/cli/commands/request_commands/get_command.py
@@ -2,43 +2,42 @@ from tqdm import tqdm
 from colorama import Fore
 import requests
 import time
-def main(params):
 
-    if 'get' in params.request:
+
+def main(params):
+    if 'GET' in params.request:
         try:
             response = requests.get(params.url)
             response_decoded = response.json()
 
-  
-
             ###############################################
             #######          ALL IS OK            #########
             ###############################################
-          
+
             if response.status_code >= 200 and response.status_code < 299:
-                
+
                 pbar = tqdm(response_decoded)
                 for i in pbar:
                     time.sleep(0.1)
                     pbar.set_description(f'Loading data: {i}')
 
-
-                if params.verbose: #If verbose is true in params print the response
+                if params.verbose:  # If verbose is true in params print the response
                     print(response_decoded)
                 print(Fore.GREEN + f'\n Succes and exit with {response.status_code} \n')
 
-    
+
 
             ###############################################
             ####          ERROR STATUS CODE          ######
             ###############################################
-                
+
             elif response.status_code >= 400 or response.status_code >= 599:
-                print(Fore.YELLOW  + f'\n Not found exit with {response.status_code} \n response: {response_decoded} \n ' )
+                print(
+                    Fore.YELLOW + f'\n Not found exit with {response.status_code} \n response: {response_decoded} \n ')
                 return response.status_code
         except:
             print(Fore.RED + "\n Error was ocurred, check the url \n ")
-            return 500
+            return
 
 
 if __name__ == '__main__':
